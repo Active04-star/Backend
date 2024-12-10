fa30a83341748dfc6e2815e9c2e3092735be5d38
tomas_fernandez <fernandeztomas735@gmail.com>
2024-12-10 16:34:18 -0300

    create dtos

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './helpers/http-exception.filter';
 
 async function bootstrap() {
-//  
-  const app = await NestFactory.create(AppModule);
 
+  const app = await NestFactory.create(AppModule);
+//
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
