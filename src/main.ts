985f269d9ca3dc1745f3473d2d455af5529235fd
tomas_fernandez <fernandeztomas735@gmail.com>
2024-12-10 16:45:26 -0300

    dto

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './helpers/http-exception.filter';
-//
-async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
+async function bootstrap() {
 //
+  const app = await NestFactory.create(AppModule);
+
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
