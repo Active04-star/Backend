08b8daac20d6e80205e7ef2a353fb30e230441cc
Joshua-frDev <elvisteck046@outlook.com>
2024-12-11 12:05:57 -0500

    Fixed/ Swagger y exportaciones para sportcenter

--- src/modules/sport-center/sport-center.module.ts
+++ src/modules/sport-center/sport-center.module.ts
@@ -6,11 +6,12 @@ import { SportCenterService } from "./sport-center.service";
 import { SportCenterRepository } from "./sport-center.repository";
 import { Photos } from "src/entities/photos.entity";
 import { UserModule } from "../user/user.module";
+import { User } from "src/entities/user.entity";
 
 @Module({
-    imports:[UserModule,TypeOrmModule.forFeature([SportCenter,Photos])],
-    controllers:[SportCenterController],
-    providers:[SportCenterService,SportCenterRepository],
-    exports:[],
+    imports: [UserModule, TypeOrmModule.forFeature([SportCenter, Photos, User])],
+    controllers: [SportCenterController],
+    providers: [SportCenterService, SportCenterRepository],
+    exports: [],
 })
-export class Sport_Center_Module{}
+export class Sport_Center_Module { }
