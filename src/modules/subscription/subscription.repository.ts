c75b3a7b5bbfd40a5b72ebbd4ec47918431e886d
Fabrizio9898 <fabrizioandrade989@gmail.com>
2024-12-20 13:37:42 -0300

    webhook

--- src/modules/subscription/subscription.repository.ts
+++ src/modules/subscription/subscription.repository.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Subscription } from 'src/entities/subscription.entity';
+import { Subscription_Payment } from 'src/entities/subscriptionPayment.entity';
+import { User } from 'src/entities/user.entity';
+import { CreateSubscriptionDto } from 'src/dtos/subscription/createSubscription.dto';
 
 @Injectable()
 export class Subscription_Repository{
+ 
   constructor(
     @InjectRepository(Subscription)
     private readonly subscriptionRepository:Repository<Subscription>
   ) {}
 
+
+  async createSubscription(data: CreateSubscriptionDto):Promise<Subscription|undefined> {
+    const created_subscription:Subscription=await this.subscriptionRepository.save(this.subscriptionRepository.create(data))
+
+    return created_subscription === null ? undefined : created_subscription;
+
+  }
 
 
 }
--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -1,17 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Subscription } from 'src/entities/subscription.entity';
-import { Repository } from 'typeorm';
+import { Subscription_Payment } from 'src/entities/subscriptionPayment.entity';
+import { User } from 'src/entities/user.entity';
+import { ApiStatusEnum } from 'src/enums/HttpStatus.enum';
+import { ApiError } from 'src/helpers/api-error-class';
+import { Subscription_Repository } from './subscription.repository';
+import { SubscriptionStatus } from 'src/enums/subscriptionStatus.enum';
 
 @Injectable()
 export class Subscription_Service {
   constructor(
     @InjectRepository(Subscription)
-    private readonly subscriptionRepostory: Repository<Subscription>, 
+    private readonly subscriptionRepostory: Subscription_Repository,
   ) {}
 
+  async createSubscription(
+    subscriptionPayment: Subscription_Payment,
+    user: User,
+  ):Promise<Subscription> {
+    const subscriptionData = {
+      status: SubscriptionStatus.ACTIVE,
+      price: subscriptionPayment.amount.toNumber(),
+      startDate: new Date(), // Fecha actual
+      endDate: new Date(new Date().setFullYear(new Date().getFullYear() + 1)), // Un año después
+      autoRenew: true,
+      userId: user.id,
+    };
+    const created_subscription =
+      await this.subscriptionRepostory.createSubscription(
+        subscriptionData,
+      );
 
-  async createSubscription(){
-    
+    if (created_subscription === undefined) {
+      throw new ApiError(
+        ApiStatusEnum.SUBSCRIPTION_PAYMENT_FAILED,
+        BadRequestException,
+      );
+    }
+
+    return created_subscription;
   }
 }
