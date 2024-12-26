f76ee0f9bd116e32599725a1fbe35648ba5cd4ac
Fabrizio9898 <fabrizioandrade989@gmail.com>
2024-12-26 20:21:11 -0300

    payment module

--- src/dtos/payment/createPayment.dto.ts
+++ src/dtos/payment/createPayment.dto.ts
@@ -25,5 +25,10 @@ export class createPaymentDto {
     @IsString()
     @IsUUID()
     fieldId: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsUUID()
+    reservationId: string;
 }
 
--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -7,9 +7,11 @@ import { Payment_Controller } from './payment.controller';
 import { Subscription_Payment } from 'src/entities/subscriptionPayment.entity';
 import { Payment_History } from 'src/entities/payment_hisotry.entity';
 import { UserModule } from '../user/user.module';
+import { Field_Module } from '../field/field.module';
+import { Reservation } from 'src/entities/reservation.entity';
 
 @Module({
-  imports: [ UserModule,TypeOrmModule.forFeature([Payment,Subscription_Payment,Payment_History])],
+  imports: [Field_Module, UserModule,TypeOrmModule.forFeature([Payment,Subscription_Payment,Payment_History,Reservation])],
   controllers: [Payment_Controller],
   providers: [ Payment_Service, Payment_Repository],
   exports: [Payment_Service],
