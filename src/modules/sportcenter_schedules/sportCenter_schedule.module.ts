a69aa7e2b57347dd4ecd849bbd4545b07604a8b7
Fabrizio9898 <fabrizioandrade989@gmail.com>
2025-01-07 20:26:41 -0300

    fix import

--- src/modules/sportcenter_schedules/sportCenter_schedule.module.ts
+++ src/modules/sportcenter_schedules/sportCenter_schedule.module.ts
@@ -4,6 +4,7 @@ import { SportCenter_Module } from '../sport-center/sport-center.module';
 import { SportCenter_Schedule } from 'src/entities/sportcenter_schedules.entity';
 import { SportCenter_Schedules_Controller } from './sportCenter_schedules.controller';
 import { SportCenter_Schedule_Service } from './sportCenter_scheudle.service';
+import { SportCenter_Schedule_Repository } from './sportCenter_scheudle.repository';
 
 @Module({
   imports: [
@@ -11,7 +12,7 @@ import { SportCenter_Schedule_Service } from './sportCenter_scheudle.service';
     TypeOrmModule.forFeature([SportCenter_Schedule]),
   ],
   controllers: [SportCenter_Schedules_Controller],
-  providers: [SportCenter_Schedule_Service],
+  providers: [SportCenter_Schedule_Service,SportCenter_Schedule_Repository],
   exports: [],
 })
 export class SportCenter_Schedule_Module {}
