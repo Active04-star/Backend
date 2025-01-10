2835aae0b7b9c432cb52b5bdd2c1f770da0979fc
Fabrizio9898 <fabrizioandrade989@gmail.com>
2025-01-09 23:07:42 -0300

    update field service\

--- src/modules/uploads/upload.module.ts
+++ src/modules/uploads/upload.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-// import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import { CloudinaryService } from 'src/config/cloudinary.config';
 
 @Module({
-  // controllers: [UploadController],
   providers: [UploadService, CloudinaryService],
   exports: [UploadService]
 })
