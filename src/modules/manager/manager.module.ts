7ca2150fa28fb1242f836304bb46e57f639415e4
Fabrizio9898 <fabrizioandrade989@gmail.com>
2025-01-14 00:46:37 -0300

    se envia reservaciones a manager

--- src/modules/manager/manager.module.ts
+++ src/modules/manager/manager.module.ts
@@ -6,19 +6,16 @@ import { ManagerService } from './manager.service';
 import { SportCenter_Module } from '../sport-center/sport-center.module';
 import { Field_Module } from '../field/field.module';
 import { UserModule } from '../user/user.module';
-import { Sport_Cateogry_Module } from '../sport-category/sport-category.module';
 import { Field } from 'src/entities/field.entity';
 import { Reservation } from 'src/entities/reservation.entity';
-import { SportCenterRepository } from '../sport-center/sport-center.repository';
 
 @Module({
   imports: [
-    Sport_Cateogry_Module,
     UserModule,
     SportCenter_Module, 
     Field_Module, 
-    TypeOrmModule.forFeature([SportCenter, Field, Reservation])],
+    TypeOrmModule.forFeature([SportCenter, Field,Reservation])],
   controllers: [ManagerController],
-  providers: [ManagerService, SportCenterRepository],
+  providers: [ManagerService],
 })
 export class ManagerModule {}
